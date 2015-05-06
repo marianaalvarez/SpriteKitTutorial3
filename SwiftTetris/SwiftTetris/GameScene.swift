8e17fe395d9942b5168478eba8e540e8deee6e0f
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-05-06 09:08:57 -0300

    Commit inicial.

---
+++ SwiftTetris/SwiftTetris/GameScene.swift
@@ -0,0 +1,31 @@
+//
+//  GameScene.swift
+//  SwiftTetris
+//
+//  Created by Mariana Alvarez on 06/05/15.
+//  Copyright (c) 2015 Mariana Alvarez. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        addChild(background)
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ SwiftTetris/SwiftTetris/GameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GameViewController.swift
+//  SwiftTetris
+//
+//  Created by Mariana Alvarez on 06/05/15.
+//  Copyright (c) 2015 Mariana Alvarez. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
